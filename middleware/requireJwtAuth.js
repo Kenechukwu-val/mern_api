70493734d15a88d03ff8ac1941cc807b8bc17ad7
Kenechukwu Nwafor <38596033+Kenechukwu-val@users.noreply.github.com>
2021-03-01 09:51:16 +0100

    modified the requireJwtAuth middleware

--- middleware/requireJwtAuth.js
+++ middleware/requireJwtAuth.js
@@ -1,5 +1,7 @@
 const Passport = require('passport')
 
-const requireJwtAuth = Passport.authenticate('jwt', { session: false })
+const requireJwtAuth = () => {
+    Passport.authenticate('jwt', { session: false })
+}
 
 module.exports = requireJwtAuth
